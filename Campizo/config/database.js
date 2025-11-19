233e4e350ca568d772d84f2933b4ff3ad524244b
HrishikeshT733 <hrishikeshtholbare@gmail.com>
2025-11-19 17:56:30 +0530

    added atlas mongodb cloud url

--- Campizo/config/database.js
+++ Campizo/config/database.js
@@ -1,3 +1,6 @@
 module.exports = {
-  url: process.env.MONGODB_URI || 'mongodb://localhost:27017/campizo'
+  // url: process.env.MONGODB_URI || 'mongodb://localhost:27017/campizo'
+
+url: process.env.MONGODB_URI || 'mongodb+srv://hrishikesh:hrishikesh@cluster0.kiocuci.mongodb.net/?appName=Cluster0'
+  
 };
